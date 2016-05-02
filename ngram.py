9bc01f9defe6ad758344c47aab522d23c4c0013e
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-02 09:40:23 -0400

    split comments into sentences using nltk

--- ngram.py
+++ ngram.py
@@ -3,23 +3,32 @@ import datetime
 import time
 import argparse
 import os
+import nltk
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import NGram
+from pyspark.ml.feature import Tokenizer, RegexTokenizer
 
 dateformat = '%Y-%m-%d'
 def ConvertToYearDate(x):
     return time.strftime(dateformat, time.gmtime(int(x)))
 
 # read and parse reddit data
-data_rdd = sc.textFile("s3n://reddit-comments/2015/*")
-jsonformat = data_rdd.filter(lambda x: len(x) > 0).map(lambda x: json.loads(x.encode('utf8')))
-etlDataMain = jsonformat.map(lambda x: [x['body'].split(' '), ConvertToYearDate(x['created_utc']), x['subreddit']])
-etlDataMain.persist(StorageLevel.MEMORY_AND_DISK_SER)
+data_rdd = sc.textFile("s3n://reddit-comments/2007/RC_2007-10")
+comments = data_rdd.filter(lambda x: len(x) > 0).map(lambda x: json.loads(x.encode('utf8')))
+comments.persist(StorageLevel.MEMORY_AND_DISK_SER)
+# split comments into sentences
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+rdd = comments.flatMap(lambda comment: [[comment['created_utc'], comment['subreddit'], sentence] for sentence in sent_detector.tokenize(comment['body'].strip())])
 
-sentenceData = sqlContext.createDataFrame(etlDataMain, ["body", "date","subreddit"])
+sentenceDataFrame = sqlContext.createDataFrame(rdd, ["date","subreddit", "sentence"])
+tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
+wordsDataFrame = tokenizer.transform(sentenceDataFrame)
+for words_label in wordsDataFrame.select("words").take(3):
+    print(words_label)
+regexTokenizer = RegexTokenizer(inputCol="sentence", outputCol="words", pattern="\\W")
 
 # remove words that occur frequently such as "a", "the"
-remover = StopWordsRemover(inputCol="body", outputCol="filtered")
+remover = StopWordsRemover(inputCol="words", outputCol="filtered")
 remover.transform(sentenceData).show(truncate=False)
 
 # generate all 3-grams
