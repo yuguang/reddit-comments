8a1975bdd00c740962f0bd3de5829e9c6d3c3a46
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-09 14:49:40 -0400

    add elastic search storage

--- ngram.py
+++ ngram.py
@@ -10,9 +10,10 @@ from pyspark.ml.feature import Tokenizer, RegexTokenizer
 from pyspark import SparkContext, SparkConf, StorageLevel
 from pyspark.sql import SQLContext
 from timeconverter import *
+from storage import ElasticSearch
 
-CASSANDRA_WAIT = 5
-QUERY_WAIT = 0.001
+PARTITIONS = 500
+THRESHOLD = 100
 
 nodes = []
 
@@ -33,60 +34,16 @@ def cleanSentence(s):
     pattern = '|'.join([Url,ShortUrl,Number,Image,TagsLt,TagsGt,TagsAmps,TagsQuote,TagsTilde,TagsDash,TagsHtml])
     text = re.sub(re.compile(pattern), '*', s.lower())
     return filter(lambda x: not(x in '.,?![]:;\/\\()"{}-$%^&*'), text)
-    
-def saveNgrams(ngramcount, rdditer, table, async=True, debug=False):
-    if debug:
-        for datatuple in rdditer:
-            print datatuple
-        return
-    from cassandra.cluster import Cluster
-    CassandraCluster = Cluster(nodes)
-
-    success = False
-    #try to reconnect if connection is down
-    while not success:
-        try:
-            session = CassandraCluster.connect(keyspace)
-            session.default_timeout = 60
-            success = True
-        except:
-            success = False
-            time.sleep(CASSANDRA_WAIT)
-
-    query = "INSERT INTO %s (ngram, subreddit, time_bucket, date, count, percentage) VALUES (?, ?, ?, ?, ? ,?)" %(table,)
-    prepared = session.prepare(query)
-
-    timeConverter = TimeConverter()
-    for datatuple in rdditer:
-        # ('2007-10-23', (126827, [u'politics', u'term terrorism clearly', 1]))
-        date = datatuple[0]
-        time_bucket = timeConverter.toTimebucket(date)
-        
-        total = float(datatuple[1][0])
-        subreddit = str(datatuple[1][0][0])
-        ngram = str(datatuple[1][0][1])
-        count = int(datatuple[1][0][2])
-        percentage = float(count) / total
-
-        bound = prepared.bind((ngram, subreddit, time_bucket, date, count, percentage))
-        if async:
-            session.execute_async(bound)
-            time.sleep(QUERY_WAIT)
-        else:
-            session.execute(bound)
-
-    session.shutdown()
 
 if __name__ == "__main__":
     timeConverter = TimeConverter()
     conf = SparkConf().setAppName("reddit")
     conf.set('spark.serializer', 'org.apache.spark.serializer.KryoSerializer')
-    sc = SparkContext(conf=conf, pyFiles=['utils.py', 'timeconverter.py'])
+    sc = SparkContext(conf=conf, pyFiles=['utils.py', 'timeconverter.py', 'storage.py'])
     sqlContext = SQLContext(sc)
     # read and parse reddit data
-    data_rdd = sc.textFile("file:///mnt/s3/2012/RC_2012-10")
+    data_rdd = sc.textFile("s3n://reddit-comments/2007/RC_2007-10")
     comments = data_rdd.filter(lambda x: len(x) > 0).map(lambda x: json.loads(x.encode('utf8')))
-
     # split comments into sentences
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
     rdd = comments.flatMap(lambda comment: [[comment['created_utc'], comment['subreddit'], cleanSentence(sentence)] for sentence in sent_detector.tokenize(comment['body'].strip())])
@@ -94,6 +51,7 @@ if __name__ == "__main__":
     sentenceDataFrame = sqlContext.createDataFrame(rdd, ["date","subreddit", "sentence"])
     tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
     wordsDataFrame = tokenizer.transform(sentenceDataFrame)
+    rdd.unpersist()
     #for words_label in wordsDataFrame.select("words").take(3):
     #    print(words_label)
 
@@ -113,10 +71,12 @@ if __name__ == "__main__":
                     .flatMap(lambda comment: [Row(date=comment['date'], subreddit=comment['subreddit'], ngram=ngram) for ngram in comment['ngrams']])
 
         # count the occurrence of each ngram by date and subreddit
-        ngramCounts = ngramRDD.map(lambda x: ((x['date'], x['subreddit'], x['ngram']), 1)).reduceByKey(lambda x, y: x + y) \
+        ngramCounts = ngramRDD.map(lambda x: ((x['date'], x['subreddit'], x['ngram']), 1)).reduceByKey(lambda x, y: x + y, PARTITIONS) \
                     .map(lambda x: (x[0][0], [x[0][1], x[0][2], x[1]]))
         # calculate ngram totals by day
-        ngramTotals = ngramRDD.map(lambda x: (x['date'], 1)).reduceByKey(lambda x, y: x + y)
+        ngramTotals = ngramRDD.map(lambda x: (x['date'], 1)).reduceByKey(lambda x, y: x + y, PARTITIONS)
 
-        ngramTotals.join(ngramCounts.filter(lambda x: x[1][2] > 1)).foreachPartition(lambda x: saveNgrams(ngram_length, 'ngrams', x, True, True))
+        db = ElasticSearch()
+        ngramCounts.filter(lambda x: x[3] > THRESHOLD).foreachPartition(lambda x: db.saveNgramCounts(ngram_length, x))
+        ngramTotals.filter(lambda x: x[1] > THRESHOLD).foreachPartition(lambda x: db.saveTotalCounts(ngram_length, x))
 
--- storage.py
+++ storage.py
@@ -1,4 +1,3 @@
-from django.db import IntegrityError
 
 class Mysql():
     def connect(self):
@@ -11,6 +10,7 @@ class Mysql():
     def saveSubredditCounts(self, month, rdd):
         self.connect()
         from reddit.models import Subreddit
+        from django.db import IntegrityError
         for line in rdd:
             d = Subreddit(month=month.replace('RC_', ''), count=line[1], name=line[0])
             try:
@@ -21,12 +21,106 @@ class Mysql():
     def saveDomains(self, month, rdd):
         self.connect()
         from reddit.models import Domain
+        from django.db import IntegrityError
         for line in rdd:
             d = Domain(month=month.replace('RC_', ''), count=line[1], name=line[0])
             try:
                 d.save()
             except IntegrityError:
                 pass
+
+class ElasticSearch():
+    def connect(self):
+        from porc import Client
+        import os
+        self.client = Client(os.environ['ORC_API_KEY'])
+
+    def saveNgramCounts(self, ngram_length, rdd, test=False):
+        self.connect()
+        collection = 'subreddit_ngram_count'
+        if test:
+            collection = collection + '-test'
+        with self.client.async() as c:
+            futures = []
+            for line in rdd:
+                date, subreddit, ngram, count = line
+                key = map(lambda x: str(x), [ngram_length, date]).join('-')
+                futures.append(c.post(collection, key, {
+                    "subreddit": subreddit,
+                    "ngram": ngram,
+                    "count": count,
+                }))
+            # block until they complete
+            responses = [future.result() for future in futures]
+            # ensure they succeeded
+            [response.raise_for_status() for response in responses]
+
+    def saveTotalCounts(self, ngram_length, rdd, test=False):
+        self.connect()
+        collection = 'ngram_total'
+        if test:
+            collection = collection + '-test'
+        with self.client.async() as c:
+            futures = []
+            for line in rdd:
+                date, count = line
+                key = map(lambda x: str(x), [ngram_length, date]).join('-')
+                futures.append(c.post(collection, key, {
+                  "count": count,
+                }))
+            # block until they complete
+            responses = [future.result() for future in futures]
+            # ensure they succeeded
+            [response.raise_for_status() for response in responses]
+
+class Cassandra():
+    def saveNgrams(ngramcount, rdditer, table, async=True, debug=False):
+        if debug:
+            for datatuple in rdditer:
+                print datatuple
+            return
+        from cassandra.cluster import Cluster
+        import time
+        CASSANDRA_WAIT = 5
+        QUERY_WAIT = 0.001
+        NODES = []
+        CassandraCluster = Cluster(NODES)
+
+        success = False
+        #try to reconnect if connection is down
+        while not success:
+            try:
+                session = CassandraCluster.connect(keyspace)
+                session.default_timeout = 60
+                success = True
+            except:
+                success = False
+                time.sleep(CASSANDRA_WAIT)
+
+        query = "INSERT INTO %s (ngram, subreddit, time_bucket, date, count, percentage) VALUES (?, ?, ?, ?, ? ,?)" %(table,)
+        prepared = session.prepare(query)
+
+        timeConverter = TimeConverter()
+        for datatuple in rdditer:
+            # ('2007-10-23', (126827, [u'politics', u'term terrorism clearly', 1]))
+            date = datatuple[0]
+            time_bucket = timeConverter.toTimebucket(date)
+
+            total = float(datatuple[1][0])
+            subreddit = str(datatuple[1][0][0])
+            ngram = str(datatuple[1][0][1])
+            count = int(datatuple[1][0][2])
+            percentage = float(count) / total
+
+            bound = prepared.bind((ngram, subreddit, time_bucket, date, count, percentage))
+            if async:
+                session.execute_async(bound)
+                time.sleep(QUERY_WAIT)
+            else:
+                session.execute(bound)
+
+        session.shutdown()
+
 
 import unittest
 
@@ -39,6 +133,16 @@ class TestDatabases(unittest.TestCase):
         d = Domain(month='2007-01', count=1, name='none')
         d.save()
         d.delete()
+
+class TestElastic(unittest.TestCase):
+    def test_save(self):
+        rdd = [('2007-10-29', 'reddit.com', '&gt; science disproves', 1),
+            ('2007-10-16', 'reddit.com', 'reddit well-equipped handle', 1),
+            ('2007-10-28', 'reddit.com', 'aside removing context', 1),
+            ('2007-10-23', 'politics', 'term terrorism clearly', 1)]
+        db = ElasticSearch()
+        db.saveNgramCounts(3, rdd)
+        db.client.delete('subreddit_ngram_count-test')
 
 if __name__ == '__main__':
     unittest.main()
