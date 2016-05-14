0296425729be2619b8e48dcc49580a698014451f
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-14 12:14:36 -0400

    add connector jar to command

--- redshift/import.sh
+++ redshift/import.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#arn:aws:iam::632503203419:role/myRedshiftRole
-spark-submit --packages com.databricks:spark-redshift_2.10:0.6.0 s3tocsv.py 2008 RC_2008-01
+spark-submit --jars RedshiftJDBC41-1.1.13.1013.jar --packages com.databricks:spark-redshift_2.10:0.6.0 --executor-memory 2g --driver-memory 10g s3tocsv.py
 echo 'author,created_utc,downs,edited,gilded,score,ups,count' > header.csv
 cat header.csv export-*.csv.files/* > combined.csv
 #rm -rf export-*.csv.files
