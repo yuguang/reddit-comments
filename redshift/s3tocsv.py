04ea8a0b3dc87f0355f265108583214fb1dbaec0
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-16 13:26:59 -0400

    break down reddit comments into individual months

--- redshift/s3tocsv.py
+++ redshift/s3tocsv.py
@@ -4,6 +4,10 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.ml.feature import Tokenizer, RegexTokenizer
 import argparse, os
+import boto.s3
+
+START_YEAR = 2007
+START_MONTH = 11
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("comment-csv")
@@ -31,24 +35,35 @@ if __name__ == "__main__":
               StructField("subreddit", StringType(), True),
               StructField("subreddit_id", StringType(), True),
               StructField("ups", LongType(), True)]
+    conn = boto.s3.connect_to_region('us-west-2')
+    bucket = conn.get_bucket('reddit-comments')
 
-    rawDF = sqlContext.read.json("s3a://reddit-comments/*", StructType(fields))
-    # filter out comment entries that don't have author names
-    filteredDF = rawDF.filter(rawDF.author != '[deleted]')
-    # tokenize comment into words
-    tokenizer = Tokenizer(inputCol="body", outputCol="words")
-    wordsDataFrame = tokenizer.transform(filteredDF)
+    folders = bucket.list("","/*/")
+    folders = filter(lambda x: len(x) > 1 and len(x[1]) > 0, map(lambda x: x.name.split('/'), folders))
 
-    # count the number of words in comments
-    commentDF = wordsDataFrame.select('author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'body', explode(wordsDataFrame.words).alias("words")) \
-            .groupBy(['author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'body']) \
-            .count()
-    # write back to a redshift table
-    commentDF.select('author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'count')\
-      .write \
-      .format("com.databricks.spark.redshift") \
-      .option("url", "jdbc:redshift://{}/dev?user={}&password={}".format(os.environ['REDSHIFT_ENDPOINT'], os.environ['REDSHIFT_USERNAME'], os.environ['REDSHIFT_PASSWORD'])) \
-      .option("dbtable", "reddit_comments") \
-      .option("tempdir", "s3a://yuguang-reddit") \
-      .mode("error") \
-      .save()
+    for year, month in folders:
+        if int(year) < START_YEAR or (int(year) == START_YEAR and len(month) > 3 and int(month.split('-')[1]) < START_MONTH):
+            continue
+        print "=========================================="
+        print "reading reddit comments for ", year, month
+        print "=========================================="
+        rawDF = sqlContext.read.json("s3a://reddit-comments/{}/{}".format(year, month), StructType(fields))
+        # filter out comment entries that don't have author names
+        filteredDF = rawDF.filter(rawDF.author != '[deleted]')
+        # tokenize comment into words
+        tokenizer = Tokenizer(inputCol="body", outputCol="words")
+        wordsDataFrame = tokenizer.transform(filteredDF)
+
+        # count the number of words in comments
+        commentDF = wordsDataFrame.select('author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'body', explode(wordsDataFrame.words).alias("words")) \
+                .groupBy(['author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'body']) \
+                .count()
+        # write back to a redshift table
+        commentDF.select('author', 'subreddit', 'created_utc', 'downs', 'gilded', 'score', 'ups', 'count')\
+          .write \
+          .format("com.databricks.spark.redshift") \
+          .option("url", "jdbc:redshift://{}/dev?user={}&password={}".format(os.environ['REDSHIFT_ENDPOINT'], os.environ['REDSHIFT_USERNAME'], os.environ['REDSHIFT_PASSWORD'])) \
+          .option("dbtable", "reddit_comments") \
+          .option("tempdir", "s3a://yuguang-reddit") \
+          .mode("error") \
+          .save()
