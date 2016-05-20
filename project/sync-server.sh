f561e776b9778cf0dac9c3204fb311d6d8107048
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-19 21:28:20 -0400

    exclude compiled python bytecode

--- project/sync-server.sh
+++ project/sync-server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -rav -e ssh --exclude='*.csv' --exclude='*.sqlite3*' --exclude='dev-python/*' --exclude='*local_settings.py' . yuguang:/home/yuguang/reddit/project
+rsync -rav -e ssh --exclude='*.pyc' --exclude='*.csv' --exclude='*.sqlite3*' --exclude='dev-python/*' --exclude='*local_settings.py' . yuguang:/home/yuguang/reddit/project
