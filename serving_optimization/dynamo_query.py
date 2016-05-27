1f23bb4579833af8ae42844d2d31ad4fda02accb
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-05-27 13:00:50 -0400

    change table

--- serving_optimization/dynamo_query.py
+++ serving_optimization/dynamo_query.py
@@ -6,11 +6,11 @@ from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
 
-table = dynamodb.Table('Ngram')
+table = dynamodb.Table('ngrams')
 
 response = table.query(
-    KeyConditionExpression=Key('Phrase').eq('first place')
+    KeyConditionExpression=Key('phrase').eq('first place')
 )
 
 for i in response['Items']:
-    print(i['Phrase'], ":", i['Date'], ":", i['Percentage'])
+    print(i['phrase'], ":", i['date'], ":", i['percentage'])
