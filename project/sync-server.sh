f8ec01c6bf67255cdf088018b6252e1252882dac
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-06-04 09:49:58 -0400

    exclude the static folder

--- project/sync-server.sh
+++ project/sync-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rsync -rav -e ssh --exclude='*.pyc' --exclude='*.csv' --exclude='*.sqlite3*' --exclude='dev-python/*' --exclude='*local_settings.py' . yuguang:/home/yuguang/reddit/project
+rsync -rav -e ssh --exclude='*.pyc' --exclude='*.csv' --exclude='*.sqlite3*' --exclude='dev-python/*' --exclude='static/*' --exclude='*local_settings.py' . yuguang:/home/yuguang/reddit/project
 rsync -rav -e ssh ./reddit/static/ yuguang:/home/yuguang/public_html/redditor.club/static
