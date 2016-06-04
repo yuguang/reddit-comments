911becaf81b974e5670cc9b820a81a7709d0cf8d
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-06-03 22:06:20 -0400

    download images from reddit to mask word clouds

--- word_cloud/imaging.py
+++ word_cloud/imaging.py
@@ -4,37 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import urllib2, json, uuid
 import requests
+from redditdownload.redditdownload import download_images
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import subprocess, os, glob, string, shutil
 
-DOWNLOADED = 'masked'
-RANDOM = 'random'
-def save_word_cloud(subreddit, frequencies, id='a'):
-    WIDTH = 1200
-    HEIGHT = 800
-    COLORS = 80
-    WAIT = 1000
-    if len(frequencies) < 100:
-        return
-    url = "https://www.reddit.com/r/{}/top/.json?limit=200&t=all".format(subreddit)
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'my_unique_reddit_downloader' + id)]
-    response = opener.open(url)
-    payload = json.loads(response.read())
+def save_word_cloud(subreddit, frequencies, stopwords=STOPWORDS):
+    WIDTH = 800
+    HEIGHT = 600
+    COLORS = 255 * 2
+    MIN_SCORE = str(300)
+    NUM_PHOTOS = str(15)
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
     try:
-        i = 0
-        if not 'preview' in payload['data']['children'][i]['data']:
-            i += 1
-        url = payload['data']['children'][i]['data']['preview']['images'][0]['source']['url']
-        # download and open image
-        image_file = opener.open(url)
-        # generate random uuid for image filename
-        filename = subreddit + '_mask'
-        with open(filename, 'wb') as output:
-          output.write(image_file.read())
-        image = Image.open(filename)
-        coloring = np.array(image)
-        wc = WordCloud(background_color="white", width=WIDTH, height=HEIGHT, max_words=500, mask=coloring,
-                       max_font_size=40)
+        # download images for subreddit
+        download_images(['--score', MIN_SCORE, '--num', NUM_PHOTOS, '--sort-type', 'topall', subreddit, subreddit])
+        # get a list of downloaded file names
+        coloring = []
+        def ext_files(subreddit, ext):
+            return glob.glob(os.path.join(BASE_DIR, subreddit) + '/*.' + ext)
+        for file in ext_files(subreddit, 'jpg') + ext_files(subreddit, 'png') + ext_files(subreddit, 'gif'):
+            base_file = os.path.basename(file)
+            # get the number of colors in the image and compare
+            image = Image.open(os.path.join(BASE_DIR, subreddit, base_file))
+            if len(image.histogram()) > COLORS:
+                coloring = np.array(image)
+                break
+        shutil.rmtree(subreddit)
+        if not len(coloring):
+            raise Exception
+        wc = WordCloud(background_color="white", width=WIDTH, height=HEIGHT, max_words=500, mask=coloring, max_font_size=40, stopwords=stopwords)
         # generate word cloud
         print frequencies[:10]
         wc.generate_from_frequencies(frequencies)
@@ -46,15 +45,28 @@ def save_word_cloud(subreddit, frequencies, id='a'):
         # we could also give color_func=image_colors directly in the constructor
         plt.imshow(wc.recolor(color_func=image_colors))
         plt.axis("off")
-        type = DOWNLOADED
-    except:
+    except Exception,e:
+        print str(e)
         # take relative word frequencies into account, lower max_font_size
-        wordcloud = WordCloud(background_color="white", width=WIDTH, height=HEIGHT, max_words=500, max_font_size=40)
+        wordcloud = WordCloud(background_color="white", width=WIDTH, height=HEIGHT, max_words=500, max_font_size=40, stopwords=stopwords)
         wordcloud.generate_from_frequencies(frequencies)
         plt.imshow(wordcloud)
         plt.axis("off")
-        type = RANDOM
     fig = plt.gcf()
     # save wordcloud for subreddit
     fig.savefig('{}.png'.format(subreddit), transparent=True, dpi=300)
-    return "generated {} image for {}".format(type, subreddit)
+    return "generated image for {}".format(subreddit)
+
+
+import unittest
+
+class TestDownload(unittest.TestCase):
+    def test_sub(self):
+        save_word_cloud('cats', [('a',1),('b',2)])
+        self.assertTrue(os.path.exists('cats.png'))
+    def test_gif(self):
+        save_word_cloud('gifs', [('a',1),('b',2)])
+        self.assertTrue(os.path.exists('gifs.png'))
+
+if __name__ == '__main__':
+    unittest.main()
